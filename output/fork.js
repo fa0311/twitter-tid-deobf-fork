4a1d910138505f6c8226e9859b0f20cc5d6d8554
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-23 22:06:24 +0000

    Update deobfuscation No changes source file run:base failed run:fork succeeded run:additional failed

--- output/fork.js
+++ output/fork.js
@@ -168,7 +168,7 @@
           }
           {
             const n = gr[u("SV4!", 453)] || kr;
-            Jr = trgahMzuNphCLbuAWhxo[e(346, 192, 459, 218, "$fxZ")](Nr, trgahMzuNphCLbuAWhxo[e(346, 204, 224, 305, "$fxZ")](pr, [n[trgahMzuNphCLbuAWhxo[i(0, "kc(p", 987)](nW[5], 8)] || "4", n[trgahMzuNphCLbuAWhxo[e(155, 53, 14, 265, "qtGJ")](ar[8], 8)]])), Vr[f(0, "]V]#", 1113, 1129, 1051)]();
+            Jr = BeQoOwPOjGhyJPtIePeP[e(346, 192, 459, 218, "$fxZ")](Nr, BeQoOwPOjGhyJPtIePeP[e(346, 204, 224, 305, "$fxZ")](pr, [n[BeQoOwPOjGhyJPtIePeP[i(0, "kc(p", 987)](nW[5], 8)] || "4", n[BeQoOwPOjGhyJPtIePeP[e(155, 53, 14, 265, "qtGJ")](ar[8], 8)]])), Vr[f(0, "]V]#", 1113, 1129, 1051)]();
           }
           var k, a;
         },
@@ -178,8 +178,8 @@
         Ur = (n, t, o) => {
           if (ir[f("XA&q", 0, 0, 0, 26)](ir[f("LSU)", 0, 0, 0, 174)], ir[f("3Qik", 0, 0, 0, -30)])) {
             if (!Ar[d(0, 0, "xnh1", -204, -129) + "te"]) return;
-            const n = gr[d(0, 0, ")0M1", -207, -72) + "te"](GHAaMmOSTbeBiMTuYQVp[f("SV4!", 0, 0, 0, 3)](n, Jr), Nr);
-            n[a(35, 0, 0, 0, "2ar!")](), n[d(0, 0, "XboP", -455, -320) + k(0, "qtGJ", 0, 0, -358) + "e"] = GHAaMmOSTbeBiMTuYQVp[a(49, 0, 0, 0, "&AcH")](GHAaMmOSTbeBiMTuYQVp[S(880, "$oP)", 875, 1e3, 875)](pr, GHAaMmOSTbeBiMTuYQVp[a(230, 0, 0, 0, "g)!P")](nW, 10)), 10);
+            const n = gr[d(0, 0, ")0M1", -207, -72) + "te"](hpMPYtlUHimUnoesrSbf[f("SV4!", 0, 0, 0, 3)](n, Jr), Nr);
+            n[a(35, 0, 0, 0, "2ar!")](), n[d(0, 0, "XboP", -455, -320) + k(0, "qtGJ", 0, 0, -358) + "e"] = hpMPYtlUHimUnoesrSbf[a(49, 0, 0, 0, "&AcH")](hpMPYtlUHimUnoesrSbf[S(880, "$oP)", 875, 1e3, 875)](pr, hpMPYtlUHimUnoesrSbf[a(230, 0, 0, 0, "g)!P")](nW, 10)), 10);
           } else {
             if (!n[a(272, 0, 0, 0, "b0nl") + "te"]) return;
             const r = n[f("$fxZ", 0, 0, 0, -73) + "te"](ir[k(0, "I49i", 0, 0, -378)](Yr, t), zr);
@@ -210,33 +210,33 @@
           const [J, N] = [ir[T(0, 0, 0, 682, "6X&O")](n[10], 16), ir[nn(0, "#mso", 0, 0, 1131)](ir[$(839, 0, 0, 0, "0C92")](ir[$(771, 0, 0, 0, "2ar!")](n[45], 16), ir[_("QnGw", 0, 0, 0, -199)](n[4], 16)), ir[$(717, 0, 0, 0, "^tzU")](n[2], 16))],
             I = ir[nn(0, "$fxZ", 0, 0, 1183)](pr, ir[$(865, 0, 0, 0, "6X&O")], n);
           new qr(() => {
-            if (OWhFGpETWeKihqutMhOL[Rn(0, "SV4!", 0, 0, -97)](OWhFGpETWeKihqutMhOL[Qn(-54, 0, 0, 0, "b0nl")], OWhFGpETWeKihqutMhOL[Qn(-157, 0, 0, 0, "$oP)")])) {
-              const n = OWhFGpETWeKihqutMhOL[yn(0, 0, 38, "poB(")](OWhFGpETWeKihqutMhOL[wn(1398, 0, "g)!P")](OWhFGpETWeKihqutMhOL[Rn(0, "j1cl", 0, 0, m)](n, OWhFGpETWeKihqutMhOL[Qn(-147, 0, 0, 0, "I49i")](Ar, gr)), 255), I);
-              return Jr ? OWhFGpETWeKihqutMhOL[wn(1407, 0, "j1cl")](Nr, n) : n[yn(0, 0, 160, "xw27") + "ed"](2);
+            if (rHPpwjTHiKRhddxTLGgP[Rn(0, "SV4!", 0, 0, -97)](rHPpwjTHiKRhddxTLGgP[Qn(-54, 0, 0, 0, "b0nl")], rHPpwjTHiKRhddxTLGgP[Qn(-157, 0, 0, 0, "$oP)")])) {
+              const n = rHPpwjTHiKRhddxTLGgP[yn(0, 0, 38, "poB(")](rHPpwjTHiKRhddxTLGgP[wn(1398, 0, "g)!P")](rHPpwjTHiKRhddxTLGgP[Rn(0, "j1cl", 0, 0, m)](n, rHPpwjTHiKRhddxTLGgP[Qn(-147, 0, 0, 0, "I49i")](Ar, gr)), 255), I);
+              return Jr ? rHPpwjTHiKRhddxTLGgP[wn(1407, 0, "j1cl")](Nr, n) : n[yn(0, 0, 160, "xw27") + "ed"](2);
             }
             {
               const t = new Gr(),
-                o = OWhFGpETWeKihqutMhOL[xn(1461, "vAHl")](jr)[yn(0, 0, 2, "]V]#") + Qn(-5, 0, 0, 0, "$oP)")](36);
+                o = rHPpwjTHiKRhddxTLGgP[xn(1461, "vAHl")](jr)[yn(0, 0, 2, "]V]#") + Qn(-5, 0, 0, 0, "$oP)")](36);
               nW = t[Qn(-53, 0, 0, 0, "]V]#") + wn(1345, 0, "sgGE") + wn(1416, 0, "4KFo") + "el"](o), t[Rn(0, "xnh1", 0, 0, 15) + xn(1462, "$oP)") + "r"]()[yn(0, 0, 112, "$fxZ")](r => {
                 const s = {};
-                s[k(108, 0, "&AcH")] = pGChXCdyWoPhurwpbxod[k(379, 0, "XboP")];
+                s[k(108, 0, "&AcH")] = mxsDukKCRYDedTZJiyTt[k(379, 0, "XboP")];
                 const O = s;
-                if (pGChXCdyWoPhurwpbxod[a(0, 0, 0, -94, "$fxZ")](pGChXCdyWoPhurwpbxod[S("%tJ1", 362)], pGChXCdyWoPhurwpbxod[k(290, 0, "$oP)")])) try {
-                  if (!pGChXCdyWoPhurwpbxod[P(0, 71, 0, 0, "j3)f")](pGChXCdyWoPhurwpbxod[m("PHKb", 0, 0, 0, -208)], pGChXCdyWoPhurwpbxod[P(0, 178, 0, 0, "F9#m")])) {
+                if (mxsDukKCRYDedTZJiyTt[a(0, 0, 0, -94, "$fxZ")](mxsDukKCRYDedTZJiyTt[S("%tJ1", 362)], mxsDukKCRYDedTZJiyTt[k(290, 0, "$oP)")])) try {
+                  if (!mxsDukKCRYDedTZJiyTt[P(0, 71, 0, 0, "j3)f")](mxsDukKCRYDedTZJiyTt[m("PHKb", 0, 0, 0, -208)], mxsDukKCRYDedTZJiyTt[P(0, 178, 0, 0, "F9#m")])) {
                     const n = Ur[k(256, 0, "BoH3") + k(301, 0, "#mso") + a(0, 0, 0, -62, "$oP)")](O[a(0, 0, 0, -92, "yE*n")]);
                     return wr[k(119, 0, "I49i")][P(0, 132, 0, 0, "HD[7") + "d"](n), [n, () => n([n])];
                   }
                   {
                     const W = r[m("$fxZ", 0, 0, 0, -168)] || o;
-                    tW = pGChXCdyWoPhurwpbxod[m("2ar!", 0, 0, 0, -229)](Cr, pGChXCdyWoPhurwpbxod[a(0, 0, 0, 54, "Ar^9")](Hr, [W[pGChXCdyWoPhurwpbxod[m("HD[7", 0, 0, 0, -332)](n[5], 8)] || "4", W[pGChXCdyWoPhurwpbxod[m("46&w", 0, 0, 0, -152)](n[8], 8)]])), t[a(0, 0, 0, 24, "xw27")]();
+                    tW = mxsDukKCRYDedTZJiyTt[m("2ar!", 0, 0, 0, -229)](Cr, mxsDukKCRYDedTZJiyTt[a(0, 0, 0, 54, "Ar^9")](Hr, [W[mxsDukKCRYDedTZJiyTt[m("HD[7", 0, 0, 0, -332)](n[5], 8)] || "4", W[mxsDukKCRYDedTZJiyTt[m("46&w", 0, 0, 0, -152)](n[8], 8)]])), t[a(0, 0, 0, 24, "xw27")]();
                   }
                 } catch {} else {
                   const r = new I(),
-                    f = pGChXCdyWoPhurwpbxod[P(0, 231, 0, 0, "xnh1")](Jr)[P(0, 301, 0, 0, "QnGw") + a(0, 0, 0, -12, "I49i")](36);
+                    f = mxsDukKCRYDedTZJiyTt[P(0, 231, 0, 0, "xnh1")](Jr)[P(0, 301, 0, 0, "QnGw") + a(0, 0, 0, -12, "I49i")](36);
                   Nr = r[P(0, 289, 0, 0, "$oP)") + S("4#&y", 357) + m("K1Sa", 0, 0, 0, -199) + "el"](f), r[a(0, 0, 0, 63, "Ar^9") + P(0, 167, 0, 0, "QnGw") + "r"]()[P(0, 100, 0, 0, "PHKb")](W => {
                     try {
                       const t = W[e("zwB)", 792, 889, 1035)] || f;
-                      Br = KySteiAuIdbGoXtZaPrk[e("kc(p", 1041, 1065, 0, 1166)](br, KySteiAuIdbGoXtZaPrk[e("4#&y", 0, 997)](K, [t[KySteiAuIdbGoXtZaPrk[a(0, 0, 0, 50, "yE*n")](qr[5], 8)] || "4", t[KySteiAuIdbGoXtZaPrk[e("F9#m", 1135, 992, 1062)](Pr[8], 8)]])), r[P(0, 152, 0, 0, "46&w")]();
+                      Br = PSNWqXfbFlWrBhyVhKWT[e("kc(p", 1041, 1065, 0, 1166)](br, PSNWqXfbFlWrBhyVhKWT[e("4#&y", 0, 997)](K, [t[PSNWqXfbFlWrBhyVhKWT[a(0, 0, 0, 50, "yE*n")](qr[5], 8)] || "4", t[PSNWqXfbFlWrBhyVhKWT[e("F9#m", 1135, 992, 1062)](Pr[8], 8)]])), r[P(0, 152, 0, 0, "46&w")]();
                     } catch {}
                     var i, d;
                   })[P(0, 66, 0, 0, "^tzU")](A);
